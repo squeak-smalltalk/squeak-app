730a13770c37c9de2a252e3e5f270b35868d78b7
Fabio Niephaus <code@fniephaus.com>
2016-08-12 13:24:42 +0200

    Only copy ARMv6 VM on 32bit

--- prepare_aio.sh
+++ prepare_aio.sh
@@ -29,7 +29,9 @@ TARGET_ZIP="${TRAVIS_BUILD_DIR}/${BUNDLE_NAME}.zip"
 
 echo "...copying VMs into bundle..."
 cp -R "${TMP_DIR}/${VM_MAC}/CogSpur.app" "${APP_DIR}"
-cp -R "${TMP_DIR}/${VM_ARM6}" "${VM_ARM_TARGET}"
+if is_32bit; then
+  cp -R "${TMP_DIR}/${VM_ARM6}" "${VM_ARM_TARGET}"
+fi
 cp -R "${TMP_DIR}/${VM_LIN}" "${VM_LIN_TARGET}"
 cp -R "${TMP_DIR}/${VM_WIN}" "${VM_WIN_TARGET}"
 
