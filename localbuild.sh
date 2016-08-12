2317ba31011386660b7944e648571fcf627b4383
Tim Felgentreff <timfelgentreff@gmail.com>
2016-08-12 10:05:21 +0200

    [ci skip] fix localbuild script

--- localbuild.sh
+++ localbuild.sh
@@ -4,12 +4,18 @@ export TRAVIS_SMALLTALK_VERSION="Squeak-trunk"
 
 # On non-Travis runs, just disable codesigning, security, and the extracting
 # of the signing key
-alias codesign=echo
-alias security=echo
 export UNZIPPATH=$(which unzip)
-function friendlyunzip() {
-    $UNZIPPATH $@ || true
+function codesign() {
+    echo "No codesigning in local build"
 }
-alias unzip=friendlyunzip
+function security() {
+    echo "No security in local build"
+}
+function unzip() {
+    "$(which unzip)" $@ || true
+}
+export -f codesign
+export -f security
+export -f unzip
 
 exec ./prepare.sh
