c84b9c5f38dcf0d329977fc7635e19560a1f4766
Fabio Niephaus <code@fniephaus.com>
2016-08-17 14:11:34 +0200

    Add balloon icon which is loaded into an image

--- prepare_image.sh
+++ prepare_image.sh
@@ -21,6 +21,7 @@ fi
 unzip -q "${TMP_DIR}/base.zip" -d "${TMP_DIR}/"
 mv "${TMP_DIR}/"*.image "${TMP_DIR}/Squeak.image"
 mv "${TMP_DIR}/"*.changes "${TMP_DIR}/Squeak.changes"
+mv "${ICONS_DIR}/balloon.png" "${TMP_DIR}/balloon.png"
 
 echo "...launching, updating, and configuring Squeak..."
 "${TMP_DIR}/${VM_BUILD}/CogSpur.app/Contents/MacOS/Squeak" "-exitonwarn" \
